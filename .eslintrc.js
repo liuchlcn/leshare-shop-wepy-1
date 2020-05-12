7310fcd1df3755ca1aae11e112a8c88a71288e1f
liuchengling <liuchlcn@126.com>
2020-05-12 21:32:38 +0800

    add less dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,13 +17,14 @@ module.exports = {
     'html/html-extensions': ['.html', '.wpy']
   },
   // add your custom rules here
+  // 具体规则解释可以参考：https://www.jianshu.com/p/29ca5a6a34fd
   'rules': {
   	"semi": [0],
     'eqeqeq': [0],
     // allow paren-less arrow functions
-    'arrow-parens': 0,
+    'arrow-parens': 0, //箭头函数用小括号括起来
+    "no-dupe-args": 2, //函数参数不能重复
     // allow async-await
-    'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'space-before-function-paren': 0
